57e4eb9c1a320f02361a961eabafb19b386978fd
Ivan Shnurchenko <ivan.s@miro.com>
2021-04-06 10:13:35 +0500

    Added aliases to switch kitty themes

---
+++ .config/fish/functions/kitty_set_theme_dark.fish
@@ -0,0 +1,4 @@
+# Defined via `source`
+function kitty_set_theme_dark --wraps='kitty @ set-colors -a -c /Users/miju/.config/kitty/dexpota-kitty-themes/themes/Chalk.conf' --description 'alias kitty_set_theme_dark kitty @ set-colors -a -c /Users/miju/.config/kitty/dexpota-kitty-themes/themes/Chalk.conf'
+  kitty @ set-colors -a -c /Users/miju/.config/kitty/dexpota-kitty-themes/themes/Chalk.conf $argv; 
+end
---
+++ .config/fish/functions/kitty_set_theme_light.fish
@@ -0,0 +1,4 @@
+# Defined via `source`
+function kitty_set_theme_light --wraps='kitty @ set-colors -a -c /Users/miju/.config/kitty/dexpota-kitty-themes/themes/Material.conf' --description 'alias kitty_set_theme_light kitty @ set-colors -a -c /Users/miju/.config/kitty/dexpota-kitty-themes/themes/Material.conf'
+  kitty @ set-colors -a -c /Users/miju/.config/kitty/dexpota-kitty-themes/themes/Material.conf $argv; 
+end
