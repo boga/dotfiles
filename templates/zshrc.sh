019dbbfd3c9529ba131da052be0b1f316890d536
Ivan Shnurchenko <ivan.s@miro.com>
2024-03-25 10:48:35 +0100

    Added NVM Node version manager

--- templates/zshrc.sh
+++ templates/zshrc.sh
@@ -76,6 +76,13 @@ then
   eval "$(zoxide init zsh)"
 fi
 
+# NVM
+function nvm_init {
+  export NVM_DIR="$HOME/.nvm"
+  [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+  [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+}
+
 # SDKMAN!
 export SDKMAN_DIR=$(brew --prefix sdkman-cli)/libexec
 [[ -s "${SDKMAN_DIR}/bin/sdkman-init.sh" ]] && source "${SDKMAN_DIR}/bin/sdkman-init.sh"
